e00e4d19019569ad205f352988d0c040a74040fa
kinn <kinn@kinn.kr>
2026-05-20 19:06:33 +0900

    add: Data-W11 notion share page

---
+++ src/app/data-w11/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+import { useEffect } from "react";
+
+const Redirect = () => {
+    useEffect(() => {
+        window.location.href =
+            "https://www.notion.so/W11-FnB-35fd4d8c2f348005a92df0176ad77e03?source=copy_link";
+    }, []);
+};
+
+export default Redirect;
